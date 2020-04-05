8a36fe2c0caafe5989dfda1f7580959bd3c9efd0
allannoel20 <noel25961@gmail.com>
2020-04-05 16:48:23 +0530

    Templates

--- Blog/views.py
+++ Blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
+# This is a list of dictionaries
+posts = [
+    {
+        'author': 'Allan',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+]
 # function to handle traffic from the homepage of our blog
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'Blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'Blog/about.html', {'title': 'About'})
