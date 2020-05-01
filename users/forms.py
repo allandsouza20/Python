24879407f2b0eba35dbacad2e3340b212702ecf2
allannoel20 <noel25961@gmail.com>
2020-05-01 23:37:12 +0530

    Update User Profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):       # Here, UserRegisterForm inherits from UserCreationForm
@@ -11,3 +12,17 @@ class UserRegisterForm(UserCreationForm):       # Here, UserRegisterForm inherit
         fields = ['username', 'email', 'password1', 'password2']
 # Here the class Meta gives us a nested namespace for configurations and keeps the configurations in one place
 # and within the configuration we are saying that the model that will be affected is the user model, for example when we do a form.save(), it is going to save it to the user model
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
