7fed96e295c36ac097bb4456d4404be8e5495ccf
allannoel20 <noel25961@gmail.com>
2020-05-02 16:04:06 +0530

    Create, Update and Delete

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views         # . refers to the current directory
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 
 urlpatterns = [
     # Here, '' refers to an empty path. If it it an empty path, whenever you give the url as localhost:8000, it directs you directly to the homepage
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # from django.http import HttpResponse
 from .models import Post  # from the models file in the current package import that Post data
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 # This is a list of dictionaries
 # posts = [
 #     {
@@ -24,6 +32,57 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'Blog/home.html', context)
+
+
+class PostListView(ListView):
+    # this tells the list what model to query in order to create the list. In this case, it will be all of our posts.
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    # this tells the list what model to query in order to create the list. In this case, it will be all of our posts.
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    # this tells the list what model to query in order to create the list. In this case, it will be all of our posts.
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    # this tells the list what model to query in order to create the list. In this case, it will be all of our posts.
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:     # if the current user is the author of the post
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    # this tells the list what model to query in order to create the list. In this case, it will be all of our posts.
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:     # if the current user is the author of the post
+            return True
+        return False
 
 
 def about(request):
