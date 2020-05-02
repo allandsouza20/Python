4ef7e8ea7e373e90f5fde2439249f6c49a1d995e
allannoel20 <noel25961@gmail.com>
2020-05-02 20:01:52 +0530

    Email and Password Reset

--- Django_Projects/settings.py
+++ Django_Projects/settings.py
@@ -131,3 +131,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'      # login is the name wegave to our url pattern for the login route
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
